d560f13159a458bafd2d801ae547bc3628bba367
GilsonMelo3638 <gilson.melo@gdfnet.df.gov.br>
2023-09-26 16:54:29 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/educandoweb/course/entities/Category.java
+++ src/main/java/com/educandoweb/course/entities/Category.java
@@ -1,16 +1,11 @@
 package com.educandoweb.course.entities;
 
 import java.io.Serializable;
-import java.util.HashSet;
-import java.util.Set;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
